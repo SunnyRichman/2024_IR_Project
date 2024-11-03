7b8a106e500cd04a4dcbd971619320a106baa363
Raweerot Bhasidhchirapiroch <raweerot.bha@student.mahidol.ac.th>
2024-11-03 15:03:54 +0700

    Add some needed files

---
+++ Clients/yao-app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path")
+const app = express();
+const router = express.Router();
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+app.use('/',express.static(path.join(__dirname,'public')));
+app.use('/CSS',express.static(path.join(__dirname,'/CSS')));
+app.use('/HTML',express.static(path.join(__dirname,'/HTML')));
+// app.use('/Backend',express.static(path.join(__dirname,'../Backend')));
+
+app.use(router);
+
+    router.get('/',(req,res) => { 
+        console.log("Request at: /homepage")
+        res.statusCode = 200; // Status 200: OK
+        res.sendFile(path.join(`${__dirname}/HTML/homepage.html`))
+    })
+
+    // Every error detected on browser will be redirected to this page.
+    router.use((req,res,next) => { 
+        console.log("404: Invalid accesssed")        
+        res.statusCode = 404; 
+        
+    })
+
+/* Run Server */
+app.listen(3000, function() {console.log("Server listening at Port " + 3000);});    
