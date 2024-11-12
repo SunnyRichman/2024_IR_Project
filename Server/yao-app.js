cb3f0b9cde30cf6c54e6a69dec91328433d93680
Raweerot Bhasidhchirapiroch <raweerot.bha@student.mahidol.ac.th>
2024-11-12 15:47:50 +0700

    Modified some information in server side.

--- Server/yao-app.js
+++ Server/yao-app.js
@@ -1,3 +1,21 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
+const fs = require("fs");
 const app = express();
+
+
+
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({
+  node: 'https://localhost:9200',
+  auth: {
+    username: 'elastic',
+    password: 'FgOhLz*X3cnOs4CU0BHH'
+  },
+  tls: {
+    ca: fs.readFileSync('C:\Users\kornl\OneDrive\เดสก์ท็อป\elasticsearch-8.15.3\config\certs\http_ca.crt'),
+    rejectUnauthorized: false
+  }
+})
+
+app.listen(3100, function() {console.log("Server listening at Port " + 3100);});
